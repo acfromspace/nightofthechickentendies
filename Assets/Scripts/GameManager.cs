25c914fa1c6e2a7f68801efa48acb362d016dbd7
Nathan Danque <nathan@danque.net>
2017-12-03 19:22:02 -0800

    [+] Initial Project

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager Instance = null;
+
+    /// <summary>
+    /// Initializes the GameManager Instance
+    /// </summary>
+    private void Awake()
+    {
+        //Initialize the singelton pattern.
+        if (Instance != null) {
+            Instance = this;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
