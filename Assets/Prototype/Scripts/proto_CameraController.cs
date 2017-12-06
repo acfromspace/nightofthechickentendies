31e329c6564e9f194c8e0a03e5935420dfe4e243
Kevin Do <kevindokhoale@gmail.com>
2017-12-05 20:15:34 -0800

    Standard First Person Controller
    [+] Unity Standard Assets

---
+++ Assets/Prototype/Scripts/proto_CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class proto_CameraController : MonoBehaviour
+{
+	public GameObject _player;
+
+	public Vector3 _cameraOffset;
+
+	public Vector3 _cameraRotation;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		transform.rotation = Quaternion.Euler(_cameraRotation);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.position = _player.transform.position + _cameraOffset;
+	}
+}
