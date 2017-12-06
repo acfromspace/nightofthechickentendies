2bc6464a3b407c4c14c8003f251a26b02733a47e
Kevin Do <kevindokhoale@gmail.com>
2017-12-05 19:50:30 -0800

    Simple Player Movement
    [+] Prototype Folder

---
+++ Assets/Prototype/Scripts/proto_PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class proto_PlayerController : MonoBehaviour
+{
+
+	public float _playerSpeed;
+
+	private Rigidbody _playerRigidBody;
+	
+	void Start ()
+	{
+		_playerRigidBody = GetComponent<Rigidbody>();
+		
+	}
+	
+	void FixedUpdate ()
+	{
+		float horizontalInput = Input.GetAxis("Horizontal");
+		float verticalInput = Input.GetAxis("Vertical");
+		
+		Vector3 movement = new Vector3(horizontalInput, 0.0f, verticalInput);
+		_playerRigidBody.velocity = movement * _playerSpeed;
+	}
+}
