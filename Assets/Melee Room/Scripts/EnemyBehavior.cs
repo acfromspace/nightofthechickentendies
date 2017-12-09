b1ac47a00389f3f92209f4fe3ad21bcb73e6f92d
Xiandra <xiandradesquitado@yahoo.com>
2017-12-08 20:58:27 -0800

    WIP Slashing Mechanics(1)
    Continued to work on slashing mechanics, added single test enemy and gets destoryed after two hits

---
+++ Assets/Melee Room/Scripts/EnemyBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour {
+
+    public int health = 100;
+
+	public void TakeDamage(int Amount)
+    {
+        health -= Amount;
+        if (health <= 0)
+        {
+            Destroy();
+        }
+    }
+
+    void Destroy()
+    {
+        Destroy(this.gameObject);
+    }
+}
