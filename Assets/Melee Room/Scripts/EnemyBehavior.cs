e5edef777a2ace787517f2747b547dc56de3c301
Xiandra <xiandradesquitado@yahoo.com>
2017-12-10 17:40:57 -0800

    WIP Slashing Mechanics(2)
    Added arms for testing and closed range of enemies. Added a spawner

--- Assets/Melee Room/Scripts/EnemyBehavior.cs
+++ Assets/Melee Room/Scripts/EnemyBehavior.cs
@@ -4,7 +4,20 @@ using UnityEngine;
 
 public class EnemyBehavior : MonoBehaviour {
 
+    Transform player;
+    float RotSpeed = 3.0f, MoveSpeed = 1.0f;
     public int health = 100;
+
+    void Start()
+    {
+        player = GameObject.FindWithTag ("Player").transform;
+    }
+
+    void Update()
+    {
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(player.position - transform.position), RotSpeed * Time.deltaTime);
+        transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+    }
 
 	public void TakeDamage(int Amount)
     {
---
+++ Assets/Melee Room/Scripts/Spawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject enemy;
+    public Vector3 spawnValues;
+    public int enemyCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+
+    void Start()
+    {
+        StartCoroutine(SpawnWaves());
+    }
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < enemyCount; i++)
+            {
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(enemy, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+        }
+    }
+}
