09d95014eedebb1ba735d3035ee2b46e17ce57ec
Kevin Do <kevindokhoale@gmail.com>
2017-12-12 18:16:41 -0800

    Credits Scene

---
+++ Assets/Prototype/Scripts/CreditsController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+		StartCoroutine(Credits());
+	}
+
+	IEnumerator Credits()
+	{
+		yield return new WaitForSeconds(10);
+		Application.Quit();
+	}
+}
