4d663d20c9a048cc8eb5f10823a369c3b62f15c6
AC De Leon <andrewchristopherdeleon@gmail.com>
2018-05-30 12:31:19 -0700

    [*] Levels 1 & 2 visuals upgraded

---
+++ Assets/Plugins/Pixelplacement/iTween/Sample/LookAtPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtPlayer : MonoBehaviour {
+
+    public Transform player;
+    //public Vector3 looker;
+
+    void Start () {
+
+	}
+
+    void OnEnable()
+    {
+        //iTween.RotateTo(gameObject, iTween.Hash("x", -90.0f));
+    }
+
+    void Update () {
+        //looker = new Vector3(-90.0f, 0.0f, 0.0f);
+        iTween.LookTo(gameObject, iTween.Hash("looktarget", player, "easetype", "linear"));
+        var oldRot = transform.localRotation;
+        transform.localRotation = Quaternion.Euler(oldRot.x + -90, oldRot.y, oldRot.z);
+        //gameObject.transform.position = new Vector3(-90.0f, 0.0f, 0.0f);
+        // "axis", "y" for a quick fix in the hash parameters
+    }
+}
