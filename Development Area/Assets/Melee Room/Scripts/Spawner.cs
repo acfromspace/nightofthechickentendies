352f7bced93614f3bdbabed5b69820e980c8d7af
AC De Leon <andrewchristopherdeleon@gmail.com>
2018-09-17 20:33:08 -0700

    [*] root files organized

---
+++ Development Area/Assets/Melee Room/Scripts/Spawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public GameObject enemy;
+    public float spawnTime = 3f;
+    public Transform[] spawnPoints;
+    public float fireSpeed;
+
+    void Start()
+    {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+
+    void Spawn()
+    {
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+        var spawnenemy = (GameObject)Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+        enemy.GetComponent<Rigidbody>().velocity = transform.forward * fireSpeed;
+    }
+}
